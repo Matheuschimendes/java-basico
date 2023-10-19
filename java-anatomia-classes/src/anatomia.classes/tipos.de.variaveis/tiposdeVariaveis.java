11d1094772f1a83707452169001ee2909b4eabc7
Curso de JS <matheuschiimenndes@gmail.com>
2023-10-19 09:50:24 -0300

    estudo sobre Variaveis

---
+++ java-anatomia-classes/src/anatomia.classes/tipos.de.variaveis/tiposdeVariaveis.java
@@ -0,0 +1,17 @@
+
+/*Tipos primitivos
+ - int, byte, short, long, float, double, boolean e char
+ - Representa valores brutos.
+
+ int idade; //Tipo "int", nome "idade", com nenhum valor atríbuido
+ int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+ double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+*/
+
+
+
+public class tiposdeVariaveis {
+    public static void main(String[] args){
+        double salarioMinimo = 2500;
+    }
+}
